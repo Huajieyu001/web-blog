7c57f68159875fd21a60b6be4764ede9861430af
Huajieyu001 <331173500@qq.com>
2025-08-18 22:58:38 +0800

    cors

---
+++ blog/src/main/java/top/huajieyu001/blog/interceptor/CorsInterceptor.java
@@ -0,0 +1,30 @@
+package top.huajieyu001.blog.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Author huajieyu
+ * @Date 2025/8/18 22:29
+ * @Version 1.0
+ * @Description TODO
+ */
+public class CorsInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
